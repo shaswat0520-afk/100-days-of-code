961572fe309193ac06bc7225b7028e9e831b0cd4
saswat <shaswat0520@gmail.com>
2026-08-19 12:15:57 +0530

    Add trial C program

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+int x=10;
+int y=20;
+int r=30;
+int area=x*y;
+int perimeter=2*(x+y);
+int circle= 3.14*r*r;
+printf("area of reactangle is %d\n:",area);
+printf("perimeter of rectangle is %d\n:",perimeter);
+printf("area of circle is %d\n:",circle);
+return 0;
+}
+
+
