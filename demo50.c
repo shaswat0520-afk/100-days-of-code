0515f60ef279bba9d8f4cdf87a78fb2ae7f202b5
saswat <shaswat0520@gmail.com>
2026-09-03 11:27:56 +0530

    Add Demo 49 and Demo 50 C lab programs

---
+++ demo50.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 0; i < rows; i++) {
+        // Print leading spaces
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        
+        // Print stars
+        for (int k = 0; k < rows - i; k++) {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
